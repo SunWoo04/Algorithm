c8b639a7d3e821e80ec2e197e497d873afb45081
SunWoo04 <saintcsw1@gmail.com>
2023-07-05 14:28:10 +0900

    dfs와bfs / 그래프문제

---
+++ cpp/1260.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int>graph[1001];
+int vertex,edge;
+
+bool dfs_visited[1001];
+bool bfs_visited[1001];
+
+void dfs(int start){
+    dfs_visited[start] = true;
+    cout << start << ' ';
+    for(int i=0;i<graph[start].size();i++){
+        if(!dfs_visited[graph[start][i]]){
+            dfs(graph[start][i]);
+        }
+    }
+}
+
+void bfs(int start){
+    queue<int>q;
+    q.push(start);
+    bfs_visited[start] = true;
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
+        cout << cur << ' ';
+        for(int i=0;i<graph[cur].size();i++){
+            if(!bfs_visited[graph[cur][i]]){
+                q.push(graph[cur][i]);
+                bfs_visited[graph[cur][i]] = true;
+            }
+        }
+    }
+}
+
+int main(){
+    int init;
+    cin >> vertex >> edge >> init;
+    for(int i=0;i<edge;i++){
+        int a,b; cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    dfs(init);
+    cout << '\n';
+    bfs(init);
+    
+}
