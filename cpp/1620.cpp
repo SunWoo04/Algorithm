a792a284a03b0f2044b8f35398672b3b8d84051e
SunWoo04 <saintcsw1@gmail.com>
2023-07-06 11:30:37 +0900

    나는야 포켓몬 마스터 이다솜

--- cpp/1620.cpp
+++ cpp/1620.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cout.tie(0); cin.tie(0);
-    
+
     unordered_map<string, int>um;
     int pkm_num,question_num; cin >> pkm_num >> question_num;
     string arr[pkm_num];
@@ -15,12 +15,11 @@ int main(){
     }
     for(int i=0;i<question_num;i++){
         string question; cin >> question;
-        int temp = atoi(question.c_str());
+        int temp = atoi(question.c_str()); // atoi는 문자열이 숫자면 숫자를 출력하고 아니면 0을 출력함
         if(temp == 0){
             cout << um[question] << '\n';
         }else{
-            int kk = stoi(question);
-            cout << arr[stoi(question)-1] << '\n';
+            cout << arr[stoi(question)-1] << '\n'; // stoi는 문자열 숫자를 정수형 숫자로 바꿔줌
         }
 
     }
