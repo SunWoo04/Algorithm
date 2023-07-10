68096823ad323ca2bcafcba7d73cc4adc13af0c9
SunWoo04 <saintcsw1@gmail.com>
2023-07-10 10:31:45 +0900

    Hashing

--- cpp/15829.cpp
+++ cpp/15829.cpp
@@ -1,15 +1,16 @@
 #include <bits/stdc++.h>
+#define MOD 1234567891;
+
 using namespace std;
 int main(){
     int num; cin >> num;
-    int value=0;
-    for(int i=0;i<num;i++){
-        char temp; cin >> temp;
-        temp -= 'y';
-        for(int j=1;j<=i;j++){
-            temp *=31;
-        }
-        value += (int)temp;
+    string alfabet; cin >> alfabet;
+    long long answer = 0;
+    long long temp = 1;
+    for(int i=0;i<alfabet.length();i++){
+        alfabet[i] -= 96;
+        answer = (answer + alfabet[i]*temp)%MOD;
+        temp = (temp*31)%MOD;
     }
-    cout << value;
+    cout << answer;
 }
