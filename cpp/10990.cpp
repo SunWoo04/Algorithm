ac7ded14272d8bb7715f3352a7f382988c83788c
SunWoo04 <saintcsw1@gmail.com>
2023-08-29 13:59:57 +0900

    별 찍기 - 15

---
+++ cpp/10990.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1-i;j++){
+            cout << ' ';
+        }
+        for(int j=0;j<i*2+1;j++){
+            if(j == 0 || j == i*2)cout <<'*';
+            else cout << ' ';
+        }
+        cout << '\n';
+    }
+}
