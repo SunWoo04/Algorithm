8aee9de8a96c6bfe2b59fa07a11b7ed13990a932
SunWoo04 <saintcsw1@gmail.com>
2023-09-21 15:38:36 +0900

    돌 게임

---
+++ cpp/9655.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // int dp[1000] = {1,2,1,};
+    // int n; cin >> n;
+
+    // for(int i=3;i<n;i++){
+    //     dp[i] = dp[i-3] + 1;
+    // }
+
+    // if(dp[n-1]%2 == 1) cout << "SK";
+    // else cout << "CY";
+
+    int n; cin >> n;
+    if(n%2 == 1) cout << "SK";
+    else cout << "CY";
+}
